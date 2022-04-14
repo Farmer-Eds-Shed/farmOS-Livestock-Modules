4a65fb2a788dab9de685adb4ca3429ea0dfdfb58
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2022-04-14 14:08:52 +0100

    Add files via upload

---
+++ farm_animal_purchase/src/Plugin/Log/LogType/Animal_Purchase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\farm_animal_purchase\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+
+/**
+ * Provides the animal purchase log type.
+ *
+ * @LogType(
+ *   id = "animal_purchase",
+ *   label = @Translation("Animal Purchase"),
+ * )
+ */
+class Animal_Purchase extends FarmLogType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+
+    // Purchase Source.
+    $options = [
+      'type' => 'string',
+      'label' => $this->t('Purchase Source'),
+      'description' => $this->t('enter source details here (farm/mart etc.)'),
+      'weight' => [
+        'form' => 20,
+        'view' => 20,
+      ],
+    ];
+    $fields['purchase_source'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    return $fields;
+  }
+
+}
